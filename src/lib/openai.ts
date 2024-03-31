6cb0123b755067c573c181781be074e93f3a09f4
hungds99 <34293141+hungds99@users.noreply.github.com>
2024-03-31 18:14:31 +0700

    remove ununse function

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -3,12 +3,3 @@ import OpenAI from 'openai';
 export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-
-// // Ask OpenAI for a streaming chat completion given the prompt
-// const response = await openai.chat.completions.create({
-//   model: 'gpt-3.5-turbo',
-//   stream: true,
-//   messages,
-// });
-// // Convert the response into a friendly text-stream
-// const stream = OpenAIStream(response);
